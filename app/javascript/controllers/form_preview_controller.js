f917e8469cf3df51c56ad40263d6e576b6c2102b
Wenderson Fernandes <oldmaverick@gmail.com>
2026-02-01 18:31:33 -0300

    feature: improved publish form preview

--- app/javascript/controllers/form_preview_controller.js
+++ app/javascript/controllers/form_preview_controller.js
@@ -2,24 +2,55 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="form-preview"
 export default class extends Controller {
-  static targets = ["submitbtn"]
+    static targets = [
+        "imageInput", "imagePreview",
+        "titleInput", "titlePreview",
+        "aboutInput", "aboutPreview",
+        "typeInput", "typePreview",
+        "cityInput", "cityPreview",
+        "stateInput", "statePreview"
+    ]
 
-  // hide the submit button
-  connect() {
-    this.submitbtnTarget.hidden = true
-  }
+    previewImage() {
+        const input = this.imageInputTarget
+        const preview = this.imagePreviewTarget
+        const file = input.files[0]
 
-  // click the hidden button -> submit the form
-  remotesubmit() {
-    this.submitbtnTarget.click()
-    clearTimeout(this.timeout)
-    this.timeout = setTimeout(() => {
-      this.submitbtnTarget.click()
-    }, 500)
-  }
+        if (file) {
+            const reader = new FileReader()
 
-  // same as above, but with "debounce"
-  // remotesubmit() {
+            reader.onload = (e) => {
+                preview.src = e.target.result
+            }
 
-  // }
+            reader.readAsDataURL(file)
+        }
+    }
+
+    updateTitle() {
+        this.titlePreviewTarget.textContent = this.titleInputTarget.value
+    }
+
+    updateAbout() {
+        // For Trix editor, value might be in the input element associated or we can get text
+        // The rich_text_area helper creates a hidden input. We usually target the "trix-editor" or listen to trix-change.
+        // However, sticking to simple value update for now if it works, or using the hidden input's value.
+        // But rich text includes HTML. We probably want innerHTML for preview if safe, or textContent to strip tags.
+        // Given the prompt "about" is text, let's try direct value first.
+        // Note: Trix updates the hidden input value on change.
+        this.aboutPreviewTarget.innerHTML = this.aboutInputTarget.value
+    }
+
+    updateType() {
+        const selectedOption = this.typeInputTarget.options[this.typeInputTarget.selectedIndex]
+        this.typePreviewTarget.textContent = selectedOption.text
+    }
+
+    updateCity() {
+        this.cityPreviewTarget.textContent = this.cityInputTarget.value
+    }
+
+    updateState() {
+        this.statePreviewTarget.textContent = this.stateInputTarget.value
+    }
 }
